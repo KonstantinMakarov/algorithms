b0fd286d2eec821c271717c993a71083c6c9c363
Kanstantsin Makarau <constancy777@gmail.com>
2019-12-10 16:59:50 +0300

    Initial commit

---
+++ src/test/java/com/algorithms/BinarySearchTest.java
@@ -0,0 +1,12 @@
+package com.algorithms;
+
+import org.testng.annotations.Test;
+
+public class BinarySearchTest {
+
+    @Test
+    public void binarySearchTest() {
+
+        System.out.println("algorithms test");
+    }
+}
